d4a57ff3ea186f4edae5839cc19ea5b3183060a9
Sanjay-Harsora1985 <58841321+Sanjay-Harsora1985@users.noreply.github.com>
2021-04-03 15:51:53 +0530

    Update StudentRepository.java

--- src/main/java/com/codecraftshop/openshift/mysql/repository/StudentRepository.java
+++ src/main/java/com/codecraftshop/openshift/mysql/repository/StudentRepository.java
@@ -1,9 +1,9 @@
-package com.codecraftshop.openshift.mysql.repository;
+package com.sanjayharsora.openshift.mysql.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.codecraftshop.openshift.mysql.model.Student;
+import com.sanjayharsora.openshift.mysql.model.Student;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long>  {
