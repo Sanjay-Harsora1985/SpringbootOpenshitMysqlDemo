8b8a97c844ec3f81bb17bde7ef76470c91ba69ea
Sanjay-Harsora1985 <58841321+Sanjay-Harsora1985@users.noreply.github.com>
2021-04-03 15:52:41 +0530

    Update SpringbootOpenshitMysqlDemoApplication.java

--- src/main/java/com/codecraftshop/openshift/mysql/SpringbootOpenshitMysqlDemoApplication.java
+++ src/main/java/com/codecraftshop/openshift/mysql/SpringbootOpenshitMysqlDemoApplication.java
@@ -1,4 +1,4 @@
-package com.codecraftshop.openshift.mysql;
+package com.sanjayharsora.openshift.mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
